8dd8e0194c2a8f43cfef9dc7e282ce47fd25b8d1
Leticia Tejero Gamboa <leticia.tejero.gamboa@hotmail.com>
2023-10-29 13:54:30 -0600

    Login
    Enlace del login.html al index.html y carrito.html

--- JS/login.js
+++ JS/login.js
@@ -1,2 +1,14 @@
 const btnSingIn = document.getElementById("sign-in");
-    btnSingUp = document.getElementById("sign-up");
+      btnSingUp = document.getElementById("sign-up");
+      formRegister = document.querySelector(".register")
+      formLogin = document.querySelector(".login")
+
+btnSingIn.addEventListener("click", e => {
+    formRegister.classList.add("hide");
+    formLogin.classList.remove("hide");
+})
+
+btnSingUp.addEventListener("click", e => {
+    formLogin.classList.add("hide");
+    formRegister.classList.remove("hide");
+}) 
